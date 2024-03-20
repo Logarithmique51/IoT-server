0fe785e24745e6452de4249991ac567ef3f7e154
Logarithmique <suivengbrandon@gmail.com>
2024-03-20 02:35:33 +0100

    test

--- mqtt.js
+++ mqtt.js
@@ -2,41 +2,44 @@ const aedes = require('aedes');
 const net = require('net');
 const mqtt = require('mqtt')
 
-// Create an MQTT server instance
 const mqttServer = aedes();
 
-// Create a TCP server
 const server = net.createServer(mqttServer.handle);
 
-// Listen to the server's listening event
+const TOPIC = {
+    PING : "limitlesslogic/ping"
+}
+
 server.listen(1883,'0.0.0.0', function () {
     console.log('MQTT Broker listening on port 1883');
 });
 
-// Handle errors
 server.on('error', function (err) {
     console.log('Error:', err);
 });
 
-const brokerAddress = 'mqtt://localhost'; // Change to your MQTT broker address
-const brokerPort = 1883; // Default MQTT port
-
-// Connect to the MQTT broker
+const brokerAddress = 'mqtt://localhost'; 
+const brokerPort = 1883;
 const client = mqtt.connect(brokerAddress, { port: brokerPort });
 
-// Listen for 'connect' event
 client.on('connect', function () {
-    console.log('Connected to MQTT broker');
-    // Subscribe to all topics (wildcard)
     client.subscribe('#');
+    client.publish("maison/lampe/1234","un gros payload")
 });
 
-// Listen for 'message' event
 client.on('message', function (topic, message) {
-    console.log('Received message:', topic, message.toString());
+     switch (topic) {
+        case TOPIC.PING:
+
+             console.log(JSON.parse(message.toString()))
+            break;
+    
+        default:
+            console.log("UNKNOW TOPIC")
+            break;
+    }
 });
 
-// Handle errors
 client.on('error', function (err) {
     console.error('Error:', err);
 });
